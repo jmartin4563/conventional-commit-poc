316710b09a4d6817190f7a065865d688ba8b65ef
Jessica Lopatta <jlopatta@newrelic.com>
2023-02-22 09:31:41 -0500

    feat: add express application skeleton

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
+
+app.get('/', (req, res, next) => res.status(200).json({ message: 'OK' }))
+
+app.listen(3000, () => console.log('Server up and listening on port 3000'))
