6ab8908ec8f38e17c5cf80088eb012d3a1a78009
Jessica Lopatta <jlopatta@newrelic.com>
2023-02-23 10:31:08 -0500

    feat: add new health check endpoint under /health

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ app.use(bodyParser.json())
 
 app.get('/', (req, res, next) => res.status(200).json({ message: 'OK' }))
 
+app.get('/health', (req, res, next) => res.status(200).json({ message: 'OK' }))
+
 app.listen(4000, () => console.log('Server up and listening on port 4000'))
