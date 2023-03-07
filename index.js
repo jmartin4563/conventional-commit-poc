cd5263c86b12de0ecd6b7ad472aabe520aeb9377
Jessica Lopatta <jlopatta@newrelic.com>
2023-03-07 15:52:28 -0500

    fix: remove endpoint
    BREAKING CHANGE removed health check endpoint`

--- index.js
+++ index.js
@@ -4,6 +4,4 @@ const bodyParser = require('body-parser')
 const app = express()
 app.use(bodyParser.json())
 
-app.get('/healthz', (req, res, next) => res.status(200).json({ msg: 'OK', timestamp: Date.now(), foo: 'bar' }))
-
 app.listen(3000, () => console.log('Server ready to accept requests'))
