ef93ff662c040f9efd3702e61aba0899d50d0561
Jessica Lopatta <jlopatta@newrelic.com>
2023-03-07 09:48:09 -0500

    feat: add new field `foo` to API response body (#20)
    Take extra time here to explain why `foo` is good, how to use it,
    and any other valuable documentation for release notes

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const bodyParser = require('body-parser')
 const app = express()
 app.use(bodyParser.json())
 
-app.get('/', (req, res, next) => res.status(200).json({ msg: 'OK', timestamp: Date.now() }))
+app.get('/', (req, res, next) => res.status(200).json({ msg: 'OK', timestamp: Date.now(), foo: 'bar' }))
 
-app.get('/health', (req, res, next) => res.status(200).json({ msg: 'OK', timestamp: Date.now() }))
+app.get('/health', (req, res, next) => res.status(200).json({ msg: 'OK', timestamp: Date.now(), foo: 'bar' }))
 
 app.listen(3000, () => console.log('Server ready to accept requests'))
